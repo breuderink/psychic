5a8c0d3fd96c2d349d4157114d3df832f3eebee8
Boris Reuderink <b.reuderink@gmail.com>
2009-02-10 09:27:36 +0100

    Updated pac_plot to use new psychic functions.

--- pac_plot.py
+++ pac_plot.py
@@ -1,28 +1,15 @@
 import os, unittest
 import numpy as np
-from braindrain.bdfreader import BDFReader
-
-def status_to_events(status_array):
-  '''
-  Use the lowest 16 bits to extract events from the status channel.
-  Events are encoded as TTL pulses, no event is indicated with the value 0.
-  Returns (events, indices)
-  '''
-  status = np.asarray(status_array, int) & 0xffff # oh I love Python...
-  change_ids = np.nonzero(np.concatenate([[1], np.diff(status)]))[0]
-  events = status[change_ids]
-  return (events[np.nonzero(events)], change_ids[np.nonzero(events)])
-
+import psychic
 
 if __name__ == '__main__':
   import pylab
-  #unittest.main()
-  b = BDFReader(open(os.path.join('data', '20090204T1512_kardelen.bdf'), 'rb'))
+  b = psychic.bdfreader.BDFReader(open(os.path.join('data', '20090204T1512_kardelen.bdf'), 'rb'))
   channels = b.read_all()
   eeg = channels[:, :-1]
   eeg = channels[:256 * 60,:]
   status = channels[:, -1]
-  events, eindices = status_to_events(channels[:, -1])
+  events, eindices = psychic.preprocessing.status_to_events(channels[:, -1])
   #pylab.plot(eeg - np.mean(eeg, axis=0) + np.arange(eeg.shape[1]) * 100)
 
   for e in [1, 2, 3, 4, 5, 9]:
