26284f52f59dc44bb4fb6f2cf4cbeee5e631ccd5
Boris Reuderink <b.reuderink@gmail.com>
2009-04-17 12:54:30 +0200

    Added setup script.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='psychic',
+  version='0.1',
+  license='GPL',
+  url='http://www.borisreuderink.nl',
+  author='Boris Reuderink',
+  author_email='b.reuderink@gmail.com',
+  packages=['psychic', 'psychic.nodes', 'psychic.tests'],
+  )
