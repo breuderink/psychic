b7f577545f8e92fc0de576e3a7988d9d63d92ef3
Boris Reuderink <b.reuderink@gmail.com>
2009-06-10 14:10:10 +0200

    Moved marker utils to separate file.

---
+++ psychic/markers.py
@@ -0,0 +1,63 @@
+import numpy as np
+
+def markers_to_events(marker_array):
+  '''
+  Extract events from the array with markers.
+  Events are encoded as TTL pulses, no event is indicated with the value 0.
+  Returns (events, indices).
+  '''
+  markers = np.asarray(marker_array, int)
+  change_ids = np.flatnonzero(np.concatenate([[1], np.diff(markers)]))
+  events = markers[change_ids]
+  return (events[np.nonzero(events)], change_ids[np.nonzero(events)])
+
+def biosemi_find_ghost_markers(ys):
+  '''
+  Biosemi seems to decimate the status channel by taking the max of each
+  window. When two makers fall in the same frame after decimation, a ghost
+  marker appears, with the value of the bitwise or of the other markers.
+  This function removes ghost markers using a heuristic. 
+  THIS FUNCTION IS DANGEROUS!
+  '''
+  ys = np.asarray(ys)
+  e, ei = markers_to_events(ys)
+
+  # First, find markers that are the binary OR of their neighbours
+  pre_ghost_post = np.array([e[:-2], e[1:-1], e[2:]]).T
+  or_matches = np.hstack([False, pre_ghost_post[:, 0] | pre_ghost_post[:,-1] \
+    == pre_ghost_post[:, 1], False])
+
+  # Now check which markers are not separated with a 0
+  non_sep_matches = np.hstack(
+    [False, (ys[ei[1:-1] - 1] != 0) & (ys[ei[2:] - 1] != 0), False])
+
+  # Finally find markers that are one frame long
+  one_frame = np.hstack([np.diff(ei) == 1, False])
+
+  ghosts = or_matches & non_sep_matches & one_frame
+  return ei[ghosts]
+
+def resample_markers(markers, newlen, max_delay=0):
+  '''
+  Resample a marker stream without losing markers. max_delay specifies how
+  many frames the markers can be delayed in *target frames*. 
+  '''
+  factor = float(newlen)/len(markers)
+  e, ei = markers_to_events(markers)
+  ei = (ei * factor).astype(int)
+  old_ei = ei.copy()
+  
+  for i in range(1, len(ei)):
+    if e[i] == e[i-1]:
+      ei[i] = max(ei[i], ei[i-1] + 2)
+    else:
+      ei[i] = max(ei[i], ei[i-1] + 1)
+      
+  if len(ei) > 0:
+    assert np.max(np.abs(ei - old_ei)) <= max_delay, \
+      'Markers are delayed to much'
+    assert max(ei) < newlen, 'Delayed markers out of bounds'
+  ys = np.zeros(newlen)
+  ys[ei] = e
+  return ys
+
--- psychic/utils.py
+++ psychic/utils.py
@@ -4,16 +4,8 @@ from bdfreader import BDFReader
 from golem import DataSet, helpers
 from scipy import signal
 
-def markers_to_events(marker_array):
-  '''
-  Extract events from the array with markers.
-  Events are encoded as TTL pulses, no event is indicated with the value 0.
-  Returns (events, indices).
-  '''
-  markers = np.asarray(marker_array, int)
-  change_ids = np.flatnonzero(np.concatenate([[1], np.diff(markers)]))
-  events = markers[change_ids]
-  return (events[np.nonzero(events)], change_ids[np.nonzero(events)])
+from markers import markers_to_events, biosemi_find_ghost_markers,\
+  resample_markers
 
 def sliding_window_indices(window_size, window_step, sig_len):
   '''Returns indices for a sliding window with shape [nwindows x window_size]'''
@@ -70,32 +62,6 @@ def popcorn(f, axis, array, *args):
   # result.shape = (i, y1, y2, y3, k, l)
   return result
 
-def biosemi_find_ghost_markers(ys):
-  '''
-  Biosemi seems to decimate the status channel by taking the max of each
-  window. When two makers fall in the same frame after decimation, a ghost
-  marker appears, with the value of the bitwise or of the other markers.
-  This function removes ghost markers using a heuristic. 
-  THIS FUNCTION IS DANGEROUS!
-  '''
-  ys = np.asarray(ys)
-  e, ei = markers_to_events(ys)
-
-  # First, find markers that are the binary OR of their neighbours
-  pre_ghost_post = np.array([e[:-2], e[1:-1], e[2:]]).T
-  or_matches = np.hstack([False, pre_ghost_post[:, 0] | pre_ghost_post[:,-1] \
-    == pre_ghost_post[:, 1], False])
-
-  # Now check which markers are not separated with a 0
-  non_sep_matches = np.hstack(
-    [False, (ys[ei[1:-1] - 1] != 0) & (ys[ei[2:] - 1] != 0), False])
-
-  # Finally find markers that are one frame long
-  one_frame = np.hstack([np.diff(ei) == 1, False])
-
-  ghosts = or_matches & non_sep_matches & one_frame
-  return ei[ghosts]
-
 def bdf_dataset(fname):
   STATUS = 'Status'
   f = open(fname, 'rb')
@@ -120,30 +86,6 @@ def bdf_dataset(fname):
     log.getLogger('Psychic.bdf_dataset').warning(
       'Found ghost markers: %s' % str(zip(d.ys.flatten()[ghost], ghost)))
   return d
-
-def resample_markers(markers, newlen, max_delay=0):
-  '''
-  Resample a marker stream without losing markers. max_delay specifies how
-  many frames the markers can be delayed in *target frames*. 
-  '''
-  factor = float(newlen)/len(markers)
-  e, ei = markers_to_events(markers)
-  ei = (ei * factor).astype(int)
-  old_ei = ei.copy()
-  
-  for i in range(1, len(ei)):
-    if e[i] == e[i-1]:
-      ei[i] = max(ei[i], ei[i-1] + 2)
-    else:
-      ei[i] = max(ei[i], ei[i-1] + 1)
-      
-  if len(ei) > 0:
-    assert np.max(np.abs(ei - old_ei)) <= max_delay, \
-      'Markers are delayed to much'
-    assert max(ei) < newlen, 'Delayed markers out of bounds'
-  ys = np.zeros(newlen)
-  ys[ei] = e
-  return ys
 
 def resample_rec(d, factor, max_marker_delay=0):
   '''Resample a recording to length d.ninstances * factor'''
