210891f4429e1dec02b3e52de682611719c8daa1
Boris Reuderink <b.reuderink@gmail.com>
2010-05-03 11:32:01 +0200

    plot_scalpgrid returns list with subplots now.

--- psychic/plots.py
+++ psychic/plots.py
@@ -61,10 +61,13 @@ def plot_scalpgrid(scalps, sensors, locs=BIOSEMI_32_LOCS, width=None,
   Clim is a list containing the minimim and maximum value mapped to a color.
 
   Titles is an optional list with titles for each subplot.
+
+  Returns a list with subplots for further manipulation.
   '''
   scalps = np.asarray(scalps)
   assert scalps.ndim == 2
   nscalps = scalps.shape[0]
+  subplots = []
 
   if not width:
     width = int(min(8, np.ceil(np.sqrt(nscalps))))
@@ -75,13 +78,15 @@ def plot_scalpgrid(scalps, sensors, locs=BIOSEMI_32_LOCS, width=None,
 
   plt.clf()
   for i in range(nscalps):
-    plt.subplot(height, width, i + 1)
+    subplots.append(plt.subplot(height, width, i + 1))
     plot_scalp(scalps[i], sensors, locs, clim=clim, cmap=cmap,
       plot_sensors=False)
     if titles:
       plt.title(titles[i])
 
-  # plot colobar next to last scalp
+  # plot colorbar next to last scalp
   bb = plt.gca().get_position()
   plt.colorbar(cax=plt.axes([bb.xmax + bb.width/10, bb.ymin, bb.width/10,
-    bb.height]), ticks=np.linspace(clim[0], clim[1], 5))
+    bb.height]), ticks=np.linspace(clim[0], clim[1], 5).round(2))
+
+  return subplots
