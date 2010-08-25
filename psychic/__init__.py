20994f4c2a740bf6b86bfb066e83b0a4b8da1027
Boris Reuderink <b.reuderink@gmail.com>
2010-08-25 14:54:46 +0200

    Added load_bdf to top namespace.

--- psychic/__init__.py
+++ psychic/__init__.py
@@ -2,7 +2,7 @@
 Psychic is copyright (c) 2010 by Boris Reuderink
 '''
 from utils import sliding_window_indices, sliding_window, stft, spectrogram,\
-  bdf_dataset, get_samplerate, slice, find_segments, cut_segments
+  bdf_dataset, load_bdf, get_samplerate, slice, find_segments, cut_segments
 from markers import markers_to_events, biosemi_find_ghost_markers, \
   resample_markers
 from plots import plot_timeseries, plot_scalpgrid, BIOSEMI_32_LOCS
