fb1fc8b527061a358b4c4884555e560a252ae7f1
Boris Reuderink <b.reuderink@gmail.com>
2010-09-03 16:04:47 +0200

    Added import of SlowSphering.

--- psychic/nodes/__init__.py
+++ psychic/nodes/__init__.py
@@ -3,3 +3,4 @@ from timefreq import TFC
 from filter import Filter, OnlineFilter, Winsorize
 from window import SlidingWindow, OnlineSlidingWindow
 from wrapper import Decimate, Slice
+from nonstat import SlowSphering
